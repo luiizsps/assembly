0314b552c861ac1bec411879e1cc1eec149d880c
Luiz Passos <lsps1019@gmail.com>
2025-09-03 17:22:57 -0300

    Create counter.asm

---
+++ counter.asm
@@ -0,0 +1,15 @@
+	.data
+var_a	.word 10
+var_b	.word 9
+	.text
+	lw	$s3, var_a
+	lw	$t1, var_b
+begin:
+	addi	$s3, $s3, 0
+	addi	$t1, $t1, 99
+counter:
+	addi	$s3, $s3, 1
+	beq	$t1, $s3, begin
+	j counter
+	
+	
